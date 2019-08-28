ea2a3e80597686ea9e068e10410e20af9319c70f
JackLi <jackli900421@hotmail.com>
2019-08-28 13:02:18 +0800

    fix detail page data sync issue

--- src/store/saga.js
+++ src/store/saga.js
@@ -1,11 +1,14 @@
-import { put, take} from 'redux-saga/effects';
+import { put, take } from 'redux-saga/effects';
 
 
 export default function* getJobDetail() {
-    const action = yield take('getJobDetail');
-    const json = yield fetch(`http://localhost:3000/${action.jobDID}`)
-    .then(response => response.json(), );
-    yield put({type:'setJobDetail', payload: json.Details});
+    while (true) {
+        const action = yield take('getJobDetail');
+        const json = yield fetch(`http://localhost:3000/${action.jobDID}`)
+            .then(response => response.json());
+        yield put({ type: 'setJobDetail', payload: json.Details });
+    }
+
 }
 
 
