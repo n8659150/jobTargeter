798485dcd13b32f3b4bd0af9ac04e114f1cf9c5b
JackLi <jackli900421@hotmail.com>
2019-08-28 14:20:56 +0800

    fix dierction keep calling

--- src/pages/jdp/jdp.js
+++ src/pages/jdp/jdp.js
@@ -1,34 +1,59 @@
 import React, { Component } from 'react';
-import { Layout, Spin } from 'antd/es';
+import { PageHeader, Tag, Spin, Descriptions, Badge } from 'antd/es';
 import { connect } from 'react-redux';
 import { getJobDetail, getDirection } from '../../store/actions';
-const { Header, Content, Footer, Sider } = Layout;
+
 
 class Jdp extends Component {
 
-    constructor(props) {
-        super(props);
-    }
-
     componentDidMount() {
         this.props.getJobDetail(this.props.params.jobDID)
-        this.props.getDirection('1', '2')
+        this.props.getDirection(Math.random().toString().slice(-1), Math.random().toString().slice(-3))
     }
 
     render() {
         return (
-            <div>
+            <div style={{ margin:"10px" }}>
+                <PageHeader
+                onBack={() => window.history.back()}
+                key={this.props.userhome}
+                title="Original Location"
+                subTitle={this.props.userhome}
+                tags={<Tag color="green">Home</Tag>}
+                >
+                </PageHeader>
                 <Spin spinning={this.props.loading}>
                     {this.props.direction.route && this.props.direction.route.paths[0].duration}
                 </Spin>
-                <Layout>
-                <Sider>Sider</Sider>
-                <Layout>
-                    <Header>{this.props.currentjob.Title}</Header>
-                    <Content>{this.props.currentjob.Content}</Content>
-                    <Footer>{this.props.currentjob.Company}</Footer>
-                </Layout>
-                </Layout>
+                {/* {this.props.currentjob.Title} */}
+                <Descriptions title="" bordered>
+                    <Descriptions.Item label="Job Title">{this.props.currentjob.Title}</Descriptions.Item>
+                    <Descriptions.Item label="Employee Type">Contract to hire</Descriptions.Item>
+                    <Descriptions.Item label="Location">Annapolis Junction, MD</Descriptions.Item>
+                    <Descriptions.Item label="Date Posted">2018-04-24 18:00:00</Descriptions.Item>
+                    <Descriptions.Item label="Usage Time" span={2}>
+                    2019-04-24 18:00:00
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Status" span={3}>
+                    <Badge status="processing" text="Running" />
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Negotiated Amount">$80.00</Descriptions.Item>
+                    <Descriptions.Item label="Discount">$20.00</Descriptions.Item>
+                    <Descriptions.Item label="Official Receipts">$60.00</Descriptions.Item>
+                    <Descriptions.Item label="Config Info">
+                    Data disk type: MongoDB
+                    <br />
+                    Database version: 3.4
+                    <br />
+                    Package: dds.mongo.mid
+                    <br />
+                    Storage space: 10 GB
+                    <br />
+                    Replication_factor:3
+                    <br />
+                    Region: East China 1<br />
+                    </Descriptions.Item>
+                </Descriptions>
             </div>
         )
     }
@@ -37,7 +62,8 @@ class Jdp extends Component {
         return {
             currentjob: state.currentjob,
             direction: state.direction,
-            loading: state.loading
+            loading: state.loading,
+            userhome: state.userhome
         }
     }
 
--- src/pages/jrp/jrpUI.js
+++ src/pages/jrp/jrpUI.js
@@ -6,17 +6,19 @@ const jrpUI = (props) => {
 
    <div style={{ margin: '10px' }}>
        <PageHeader
+        key={props.userhome}
         title="Original Location"
         subTitle={props.userhome}
         tags={<Tag color="green">Home</Tag>}
         extra={[
             <Input
+                key="1"
                 placeholder={props.placeholder}
                 style={{ width: '300px', marginRight: '10px' }}
                 onChange={props.updateKeyword}
                 value={props.keywords}
             />,
-            <Button type="primary" onClick={props.search.bind(this,props.keywords)}>Go!</Button>
+            <Button key="2" type="primary" onClick={props.search.bind(this,props.keywords)}>Go!</Button>
             ]}>
     </PageHeader>
 
