d4972115d9471552326c2a176243cf9ed02acbcd
JackLi <jackli900421@hotmail.com>
2019-08-28 12:58:15 +0800

    fix search feature

--- src/store/saga_search.js
+++ src/store/saga_search.js
@@ -1,7 +1,11 @@
 import { put, take } from 'redux-saga/effects';
 export default function* getJobResult() {
-const action = yield take('search');
-const result = yield fetch(`http://localhost:3000/all?q=${action.keywords}`)
-        .then(response => response.json());
-yield put({type:'getJobResult', payload: result})
+        while (true) {
+                const action = yield take('search');
+                const result = yield fetch(`http://localhost:3000/all?q=${action.keywords}`)
+                        .then(response => response.json());
+                yield put({ type: 'getJobResult', payload: result })
+
+        }
+
 }
